e84370a359ccabb3ff2368ee0ad7d9cffebd9cc8
Mark <developerzero7@gmail.com>
2019-02-12 13:32:10 +0800

    fix grammar issue

--- models/workPage.js
+++ models/workPage.js
@@ -1,11 +1,18 @@
-module.exports = [{
-  header: `my background`,
-  subHeader: `Frontend Web Developer 2017 – present`,
-  para: [`After having studied business management at the Manado State Polytechnic but did not graduate. I began learning web development for almost two years which primary focus on front-end web development.`, `Since then I diving into an online course including completing the Front End Techdegree at treehouse.`, `Currently, I am looking for a job that will enhance my abilities and skills, especially for a work from home environment. I find home-based/online jobs fulfilling and continually hone my organizational and professional skills.`, `I am looking forward to work with a company that will provide best practices and motivation. I am an aspiring team worker, a hardworking and dedicated professional who wants to meet the challenges posed in the industry and to contribute towards the growth of the organization.`],
-  img: `images/logos/JS-Logo-Design.jpg`,
-  pullImg: 'img-right',
-  altText: `about me`,
-  btnPara: `My GitHub Pages`,
-  internalLink: false,
-  link: `https://excell07.github.io/`,
-}]
+module.exports = [
+  {
+    header: `my background`,
+    subHeader: `Frontend Web Developer 2017 – present`,
+    para: [
+      `After having studied business management at the Manado State Polytechnic but did not graduate. I began learning web development for almost two years which primary focus on front-end web development.`,
+      `Since then I dived into an online course including completing the Front End Techdegree at treehouse.`,
+      `Currently, I am looking for a job that will enhance my abilities and skills, especially for a work from home environment. I find home-based/online jobs fulfilling and continually hone my organizational and professional skills.`,
+      `I am looking forward to work with a company that will provide best practices and motivation. I am an aspiring team worker, a hardworking and dedicated professional who wants to meet the challenges posed in the industry and to contribute towards the growth of the organization.`
+    ],
+    img: `images/logos/JS-Logo-Design.jpg`,
+    pullImg: "img-right",
+    altText: `about me`,
+    btnPara: `My GitHub Pages`,
+    internalLink: false,
+    link: `https://excell07.github.io/`
+  }
+];
